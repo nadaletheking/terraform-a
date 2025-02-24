dafa08e4e50cd270b2cde2693c837302be31cad0
nadaletheking <you@example.com>
2025-02-24 14:59:17 +0200

    first commit (with user group bug)

--- main.tf
+++ main.tf
@@ -1,1 +1,53 @@
-# your terraform script
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "il-central-1"
+}
+
+provider "tls" {
+
+}
+
+
+# do not use this in production,
+# the private key is saves as plain text in the terraform state file
+resource "tls_private_key" "tls_pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.tls_pk.public_key_openssh
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-00e448b3f42f8156f"
+  instance_type   = "t3.micro"
+  key_name        = aws_key_pair.generated_key.key_name
+  security_groups = [aws_security_group.ec2_security_group.name]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "local_sensitive_file" "pem_file" {
+  filename             = "${path.module}/output-files/pk.pem"
+  file_permission      = "600"
+  directory_permission = "700"
+  content              = tls_private_key.tls_pk.private_key_pem
+}
---
+++ security-group.tf
@@ -0,0 +1,49 @@
+# Avoid using the ingress and egress arguments of the aws_security_group resource to configure in-line rules, 
+# as they struggle with managing multiple CIDR blocks, 
+# and, due to the historical lack of unique IDs, tags and descriptions. 
+# To avoid these problems,
+# use the current best practice of the aws_vpc_security_group_egress_rule 
+# and aws_vpc_security_group_ingress_rule resources with one CIDR block per rule.
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2-security-group"
+  description = "Allow SSH, HTTP, and HTTPS access only"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.ec2_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.ec2_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.ec2_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.ec2_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
